79201e55cb2acb105bb7c725a551399c93814f85
Juntao Qiu <juntao.qiu@gmail.com>
2018-07-16 23:08:38 +0800

    refactor a little bit

--- statistic.hs
+++ statistic.hs
@@ -1,28 +1,27 @@
-module Main where
-
-    data Status = Dev | QA | Block deriving (Show)
-    type Mark = (Status, Double)
-
-    value :: Char -> Mark
-
-    value 'd' = (Dev, 0.5)
-    value 'D' = (Dev, 1.0)
-    value 'q' = (QA, 0.5)
-    value 'Q' = (QA, 1.0)
-    value 'b' = (Block, 0.5)
-    value 'B' = (Block, 1.0)
-
-let dict = [('d', ("Dev", 0.5)), ('D', ("Dev", 1.0)), ('q', ("QA", 0.5)), ('Q', ("QA", 1.0)), ('b', ("Block", 0.5)), ('B', ("Block", 1.0)) ]
+dict = [
+  ('d', ("Dev", 0.5)), 
+  ('D', ("Dev", 1.0)), 
+  ('q', ("QA", 0.5)), 
+  ('Q', ("QA", 1.0)), 
+  ('b', ("Block", 0.5)), 
+  ('B', ("Block", 1.0))
+  ]
 
 parse :: String -> [(String, Double)]
 parse expr = [y | z <- expr, (x, y) <- dict, x == z]
 
-calc :: String -> [(String, Double)]
 
-calc expr = 
-    [
-    ("QA", sum [y | (x, y) <- parse expr, x == "QA"]), 
-    ("Dev", sum [y | (x, y) <- parse expr, x == "Dev"]), 
-    ("Block", sum [y | (x, y) <- parse expr, x == "Block"]), 
-    ("Total", sum [y | (x, y) <- parse expr]) 
-]
+inStatus result status = (status, sum [y | (x, y) <- result, x == status])
+
+calc :: String -> [(String, Double)]
+calc expr = [
+  inStatus (parse expr) "QA", 
+  inStatus (parse expr) "Dev",
+  inStatus (parse expr) "Block",
+  ("Total", sum [y | (x, y) <- parse expr]) 
+  ]
+
+-- macro result field = (@field, sum [y | (x, y) <- result, x == @field)
+-- calc expr = [macro result "QA"]
+
+-- zip [1..] ["apple", "orange", "cherry", "mango"]  
