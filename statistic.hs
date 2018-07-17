a2ff5b34dd112e09a0697cd4f8cca5d960c8daf2
Juntao Qiu <juntao.qiu@gmail.com>
2018-07-17 21:39:54 +0800

    make it work with functions

--- statistic.hs
+++ statistic.hs
@@ -1,24 +1,23 @@
-dict = [
-  ('d', ("Dev", 0.5)), 
-  ('D', ("Dev", 1.0)), 
-  ('q', ("QA", 0.5)), 
-  ('Q', ("QA", 1.0)), 
-  ('b', ("Block", 0.5)), 
-  ('B', ("Block", 1.0))
-  ]
+detail :: Char -> (String, Double)
+detail 'd' = ("Dev", 0.5)
+detail 'D' = ("Dev", 1.0)
+detail 'q' = ("QA", 0.5) 
+detail 'Q' = ("QA", 1.0) 
+detail 'b' = ("Block", 0.5)
+detail 'B' = ("Block", 1.0)
+detail x = ([], 0.0)
 
 parse :: String -> [(String, Double)]
-parse expr = [y | z <- expr, (x, y) <- dict, x == z]
-
+parse expr = [detail x | x <- expr]
 
 inStatus result status = (status, sum [y | (x, y) <- result, x == status])
 
-calc :: String -> [(String, Double)]
-calc expr = [
-  inStatus (parse expr) "QA", 
-  inStatus (parse expr) "Dev",
-  inStatus (parse expr) "Block",
-  ("Total", sum [y | (x, y) <- parse expr]) 
+calc :: [(String, Double)] -> [(String, Double)]
+calc timesheet = [
+  inStatus timesheet "Dev",
+  inStatus timesheet "QA", 
+  inStatus timesheet "Block",
+  ("Total", sum [y | (x, y) <- timesheet]) 
   ]
 
 -- macro result field = (@field, sum [y | (x, y) <- result, x == @field)
